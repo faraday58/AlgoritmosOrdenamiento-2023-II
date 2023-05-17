07d114153f67baf20f50cfabf771857de45b52c2
Armando Rodríguez <jarodriguezvera@gmail.com>
2023-05-17 12:14:51 -0600

    Agregar archivos de proyecto.

---
+++ Ordenamiento.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AlgoritmosOrdenamiento_2023_II
+{
+    internal class Ordenamiento
+    {
+        public static void  Burbuja(int [] A  )
+        {
+            int N=A.Length;
+
+            for (int i=0; i < N; i++ )
+            {
+                for ( int j=0; j < N-1; j++)
+                {                   
+
+                    if (A[j] > A[j+1]  )
+                    {
+                        int Aux= A[j];
+                        A[j] = A[j + 1];
+                        A[j+1] = Aux;
+
+                    }
+                }        
+                        
+            }
+
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AlgoritmosOrdenamiento_2023_II
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce el tamaño del arreglo: ");
+            int N = int.Parse(Console.ReadLine());
+            int [] A=Genera(N);
+            Imprime(A);
+            Console.WriteLine("Arreglo Ordenado");
+
+            Ordenamiento.Burbuja(A);
+            Imprime(A);
+
+            Console.ReadLine();
+
+        }
+
+        public static int[]  Genera(int N)
+        {
+            int[] A= new int[N];
+            Random aleatorio= new Random();
+            for(int i=0; i<N; i++)
+            {
+                A[i] =(int)(1000*aleatorio.NextDouble());
+
+            }
+
+            return A;
+        }
+
+        public static void Imprime(int[] A)
+        {
+            Console.WriteLine("Imprime el Arreglo: ");
+            foreach( int a in A )
+            {
+                Console.Write(" {0} ",a);
+            }
+            Console.WriteLine("Fin del Arreglo");
+
+        }
+
+    }
+}
